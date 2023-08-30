ff13db05f873a711c37b0bdbf7555df59eab698a
Mykhaylo Chugayev <chugayev.mikhaylo@gmail.com>
2023-08-30 15:37:28 +0300

    first commit

---
+++ hw-1/dist/test1.js
@@ -0,0 +1,11 @@
+"use strict";
+function example1() {
+    let isDone = false;
+    let age = 23;
+    let firstName = "John";
+    let nothing = null;
+    let notDefined = undefined;
+    let largeNUmber = 100n;
+    let id = Symbol("id");
+    let id2 = Symbol("id");
+}
